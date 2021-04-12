8873f98fb4123d472e6c3903e63582c99210c4ca
Jeffery Wood <35505841+jwoodii@users.noreply.github.com>
2021-04-11 20:38:36 -1000

    Match Data Layout

--- app/imports/ui/pages/MatchID.jsx
+++ app/imports/ui/pages/MatchID.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
-import { Container, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
+import {Container, Form, Grid, Header, Message, Segment} from 'semantic-ui-react';
 import matchData from '../../api/stuff/matchData'
 import validator from 'validator';
 
 
 class MatchID extends React.Component {
-   
+
     /*
     Binds handleinputchange and handlesubmit so they may be called down below
     Also sets matchID to an empty string
   */
-  constructor(props){
-    super(props)
-    this.state = {
-      matchID: '', 
-      victoryC: '',
-      duration:'',
-      skillInt: '', 
-      skillString: '',
-      victoryOut: ''
+    constructor(props) {
+        super(props)
+        this.state = {
+            matchID: '',
+            victoryC: '',
+            duration: '',
+            skillInt: '',
+            skillString: '',
+            victoryOut: '',
+            victorImage: ''
+        }
+        this.matchData = new matchData(props);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    this.matchData = new matchData(props);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-    
 
-   /*
-    Shows user what they are inputting before calling 
-    an imported method getMatchData on the results
-  */
+
+    /*
+     Shows user what they are inputting before calling
+     an imported method getMatchData on the results
+   */
     handleSubmit = (event) => {
         event.preventDefault();
         const matchID = this.state.matchID;
-    
-          if(validator.isInt(matchID))
-          {
+
+        if (validator.isInt(matchID)) {
             this.setState({matchID});
 
             console.log(matchID);
@@ -44,205 +44,200 @@ class MatchID extends React.Component {
             this.getMatchSkill(parseInt(matchID));
             this.getMatchDuration(parseInt(matchID));
 
-          } 
-          else
-          {
+        } else {
             alert("Please enter an integer");
-          }
-        
-      }
+        }
 
-      /*
-    Sets the state whenever matchID is changed 
-  */
-  handleInputChange = (event) => {
-    this.setState({
-       matchID: event.target.value
-      
-      })
+    }
 
-  }
-
-  /**
-    * Gets the match data from a passed match ID and sends a GET request to the OPEN DOTA api
-    * In this case, the duration of the game is found, as well as the winner and the skill group
-    * @param x 
-    */
-   async getMatchWinner(x){
-    await fetch("https://api.opendota.com/api/matches/" + x)
-    .then(res => res.json())
-    .then((result) => {
+    /*
+  Sets the state whenever matchID is changed
+*/
+    handleInputChange = (event) => {
         this.setState({
-            isOK: true,
-            matchResult: result.data,
-            victoryC: result.radiant_win,
+            matchID: event.target.value
 
-        });
-        
-        //console.log(result.radiant_win);
-        this.victoryFind(result.radiant_win);
-    },
-        //if(response.ok){
-       // }
-       // else {
-        //    throw new Error('Something went wrong');
-       // }
-    (error) => {
-        this.setState({
-            isOK: true,
-            error
-        });
+        })
 
-    })
-}
-
-/**
-    * Gets the match data from a passed match ID and sends a GET request to the OPEN DOTA api
-    * In this case, the duration of the game is found, as well as the winner and the skill group
-    * @param x 
-    */
- async getMatchDuration(x){
-  await fetch("https://api.opendota.com/api/matches/" + x)
-  .then(res => res.json())
-  .then((result) => {
-      this.setState({
-          isOK: true,
-          matchResult: result.data,
-          duration: result.duration,
-      });
-  },
-      //if(response.ok){
-     // }
-     // else {
-      //    throw new Error('Something went wrong');
-     // }
-  (error) => {
-      this.setState({
-          isOK: true,
-          error
-      });
-
-  }
-  
-  )
-}
-/**
-    * Gets the match data from a passed match ID and sends a GET request to the OPEN DOTA api
-    * In this case, the duration of the game is found, as well as the winner and the skill group
-    * @param x 
-    */
- async getMatchSkill(x){
-  await fetch("https://api.opendota.com/api/matches/" + x)
-  .then(res => res.json())
-  .then((result) => {
-      this.setState({
-          isOK: true,
-          matchResult: result.data,
-          skillInt: result.skill
-
-      });
-
-      this.skillFind(result.skill);
-      
-  },
-      //if(response.ok){
-     // }
-     // else {
-      //    throw new Error('Something went wrong');
-     // }
-  (error) => {
-      this.setState({
-          isOK: true,
-          error
-      });
-
-  }
-  
-  )
-}
+    }
 
     /**
-    * Function to see which side won the game 
-    * @param {bool} radiantWin 
-    */
-       victoryFind(radiantWin) {
+     * Gets the match data from a passed match ID and sends a GET request to the OPEN DOTA api
+     * In this case, the duration of the game is found, as well as the winner and the skill group
+     * @param x
+     */
+    async getMatchWinner(x) {
+        await fetch("https://api.opendota.com/api/matches/" + x)
+            .then(res => res.json())
+            .then((result) => {
+                    this.setState({
+                        isOK: true,
+                        matchResult: result.data,
+                        victoryC: result.radiant_win,
+
+                    });
+
+                    //console.log(result.radiant_win);
+                    this.victoryFind(result.radiant_win);
+                },
+                //if(response.ok){
+                // }
+                // else {
+                //    throw new Error('Something went wrong');
+                // }
+                (error) => {
+                    this.setState({
+                        isOK: true,
+                        error
+                    });
+
+                })
+    }
+
+    /**
+     * Gets the match data from a passed match ID and sends a GET request to the OPEN DOTA api
+     * In this case, the duration of the game is found, as well as the winner and the skill group
+     * @param x
+     */
+    async getMatchDuration(x) {
+        await fetch("https://api.opendota.com/api/matches/" + x)
+            .then(res => res.json())
+            .then((result) => {
+                    this.setState({
+                        isOK: true,
+                        matchResult: result.data,
+                        duration: result.duration,
+                    });
+                },
+                //if(response.ok){
+                // }
+                // else {
+                //    throw new Error('Something went wrong');
+                // }
+                (error) => {
+                    this.setState({
+                        isOK: true,
+                        error
+                    });
+
+                }
+            )
+    }
+
+    /**
+     * Gets the match data from a passed match ID and sends a GET request to the OPEN DOTA api
+     * In this case, the duration of the game is found, as well as the winner and the skill group
+     * @param x
+     */
+    async getMatchSkill(x) {
+        await fetch("https://api.opendota.com/api/matches/" + x)
+            .then(res => res.json())
+            .then((result) => {
+                    this.setState({
+                        isOK: true,
+                        matchResult: result.data,
+                        skillInt: result.skill
+
+                    });
+
+                    this.skillFind(result.skill);
+
+                },
+                //if(response.ok){
+                // }
+                // else {
+                //    throw new Error('Something went wrong');
+                // }
+                (error) => {
+                    this.setState({
+                        isOK: true,
+                        error
+                    });
+
+                }
+            )
+    }
+
+    /**
+     * Function to see which side won the game
+     * @param {bool} radiantWin
+     */
+    victoryFind(radiantWin) {
         if (radiantWin == true) {
-         this.victoryOut = "Radiant Victory!";
-         //console.log(this.victoryOut);
-         //return "Radiant Victory!";
+            this.victoryOut = "Radiant Victory!";
+            this.victorImage = "https://cdn-images.win.gg/resize/w/800/h/600/format/jpg/type/progressive/fit/cover/path/news/b9b852ac7bd1776bc5ac5ce3b41d8af7/c0b830593397c256dc8269556c5512f8/original.jpg"
+            //console.log(this.victoryOut);
+            //return "Radiant Victory!";
+        } else {
+            this.victoryOut = "Dire Victory!";
+            this.victorImage = "https://www.vpesports.com/wp-content/uploads/2020/03/dire-cover.png"
+            //return "Dire Victory!";
         }
-        else {
-          this.victoryOut = "Dire Victory!";
-          //return "Dire Victory!";
-  }
-}
+    }
 
 
-      /**
-      * Function to see what the skill level of the game was
-      * @param {int} skillLevel 
-      */
-        skillFind(skillLevel) {
-           if (skillLevel == 0) {
-              this.skillString = "Below Average!"
-          }
-          else if (skillLevel == 1) {
+    /**
+     * Function to see what the skill level of the game was
+     * @param {int} skillLevel
+     */
+    skillFind(skillLevel) {
+        if (skillLevel == 0) {
+            this.skillString = "Below Average!"
+        } else if (skillLevel == 1) {
             this.skillString = "Average!"
-          }
-          else if (skillLevel == 2) {
+        } else if (skillLevel == 2) {
             this.skillString = "Above Average skill!"
-          }
-          else if (skillLevel == 3) {
+        } else if (skillLevel == 3) {
             this.skillString = "High skill!"
-          }
-          else {
+        } else {
             this.skillString = "???";
-  }
-}
+        }
+    }
 
-  render(){
-      const matchID = this.state
-      return (
-        <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-            <Grid.Column width = {4}>
-              <div class = "ui form">
-                <h1>Input MatchID Here</h1>
-                <form onSubmit = {this.handleSubmit}>
-                  <input
-                   type = "text" 
-                   placeholder = "MatchID"
-                   value = {this.state.matchID}
-                   onChange = {this.handleInputChange}
-                   />
-                <button>Search</button>
-                </form>
-              </div>
-          </Grid.Column>
-
-          <Grid.Column width = {12}>
-              <div class = "ui cards">
-                  <div class = "card">
-                      <div class = "content">
-                          <img class = "right floated ui image" src = ""/>
-                          <div class = "header">
+    render() {
+        const matchID = this.state
+        return (
+            <Grid id='landing-page' verticalAlign='middle' textAlign='center' container style={{height: '100vh'}}>
+                <Grid.Row style={{height: '5%'}}>
+                    <div className="ui form">
+                        <h1>Input MatchID Here</h1>
+                        <form onSubmit={this.handleSubmit}>
+                            <input
+                                type="text"
+                                placeholder="MatchID"
+                                value={this.state.matchID}
+                                onChange={this.handleInputChange}
+                            />
+                            <button>Search</button>
+                        </form>
+                    </div>
+                </Grid.Row>
+                <Grid.Row style={{height: '15%'}}>
+                    <Grid.Column width={10}>
+                        <b>
+                            <div className="header" className="victor">
                                 Winner: {this.victoryOut}
-                          </div>
-                          <div class = "header">
-                                Duration of Match: {(this.state.duration/60).toFixed(3)} minutes and {(this.state.duration % 60).toFixed(3)} seconds
-                          </div>
-                          <div className="header">
-                                Skill level: {this.skillString}
-                          </div>
-                          {/* <div className="header">
-                              Hero KDA: {this.state.heroKDA}
-                          </div> */}
-                      </div>
-                  </div>
-              </div>
-          </Grid.Column>
-
-        </Grid>
-      );
-  }
+                            </div>
+                        </b>
+                    </Grid.Column>
+                    <Grid.Column width={6}>
+                        <img src={this.victorImage}/>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row style={{height: '10%'}}>
+                    <div className="header" className="victor">
+                        Duration of Match: {(this.state.duration / 60).toFixed(0)} minutes
+                        and {(this.state.duration % 60).toFixed(0)} seconds
+                    </div>
+                </Grid.Row>
+                <Grid.Row style={{height: '30%'}}>
+                    <div className="header"className="victor">
+                        Skill level: {this.skillString}
+                    </div>
+                </Grid.Row>
+            </Grid>
+        );
+    }
 }
+
 export default MatchID;
